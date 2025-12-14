b1cd7ce208fbe2f12ecf833fe53fef1ca49f22f9
innnoa <banxiaqaq@gmail.com>
2025-12-14 15:25:56 +0800

    day3_0.2

--- server/src/network/tcp/tcp_server.cpp
+++ server/src/network/tcp/tcp_server.cpp
@@ -8,40 +8,39 @@ void TcpSession::start() {
   do_read(); 
 }
 
-void TcpSession::send(const std::string& data) {
+void TcpSession::send(const std::string& data) { // 一个接口，暂时没用上
   write_data_ = data;
   do_write();
 }
 
 void TcpSession::do_read() {
-  auto self = shared_from_this();
-  socket_.async_read_some(asio::buffer(buffer_),
-    [this, self](const asio::error_code& ec,
-    std::size_t bytes_transferred) {
+  auto self = shared_from_this(); // 获取自身的shared_ptr
+  socket_.async_read_some(asio::buffer(buffer_), // 注册异步读取回调，读取到的数据保存到buffer
+    [this, self](const asio::error_code& ec,std::size_t bytes_transferred) {
       if (ec) {
         return;
       }
-    write_data_.assign(buffer_.data(), bytes_transferred);
-    do_write();
+    write_data_.assign(buffer_.data(), bytes_transferred); // 将buffer中的数据转移到write_data
+    do_write(); // 调用write
     }
   );
 }
 
 void TcpSession::do_write() {
-  auto self = shared_from_this();
-  asio::async_write(socket_, asio::buffer(write_data_),
+  auto self = shared_from_this(); // 获取自身的shared_ptr
+  asio::async_write(socket_, asio::buffer(write_data_), //注册异步写入回调
     [this, self](const asio::error_code& ec, std::size_t) {
       if (ec) {
         return;
       }
-      do_read();
+      do_read(); // 调用read
     }
   );
 }
 
 TcpServer::TcpServer(asio::io_context& io, uint16_t port): 
-  io_context_(io),
-  acceptor_(io_context_, tcp::endpoint(tcp::v4(), port)) {
+  io_context_(io), // 创建一个上下文
+  acceptor_(io_context_, tcp::endpoint(tcp::v4(), port)) { // 舰艇和接受TCP连接，绑定端口
 }
 
 void TcpServer::start() { 
@@ -49,10 +48,12 @@ void TcpServer::start() {
 }
 
 void TcpServer::do_accept() {
-  acceptor_.async_accept([this](const asio::error_code& ec, tcp::socket socket) {
+  acceptor_.async_accept([this](const asio::error_code& ec, tcp::socket socket) { // 异步接受连接，注册回调函数
+      // 这里的socket无需手动创建，async_accept会自动创建
     if (!ec) {
-        std::make_shared<TcpSession>(std::move(socket))->start();
-    }
-    do_accept();
+        std::make_shared<TcpSession>(std::move(socket))->start(); 
+        // 将socket权限转移给Session,对于单个连接由TcpSession单独负责
+    } // 立即返回，非阻塞，当有客户端连接时，asio调用回调函数
+    do_accept(); // 再次创建一个新的连接，递归
   });
 }
